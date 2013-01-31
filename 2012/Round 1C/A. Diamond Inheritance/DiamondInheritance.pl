55b56a7f3eb8898b71da7ea7585f713e8f64c6c6
Sergey Dymchenko <kit1980@gmail.com>
2013-01-30 21:36:57 -0800

    Add B-Prolog solution and test for 2012/Round 1C/A. Diamond Inheritance.

---
+++ 2012/Round 1C/A. Diamond Inheritance/DiamondInheritance.pl
@@ -0,0 +1,46 @@
+% Google Code Jam
+% Round 1C 2012
+% A. Diamond Inheritance
+% https://code.google.com/codejam/contest/1781488/dashboard#s=p0
+%
+% B-Prolog 7.8#1 - http://www.probp.com/
+% Usage:
+% sed 's/^ */[/; s/ *$/]./; s/ \+/, /g' in-file | bp -g "['DiamondInheritance.pl'],main,halt" -l | tail -n +5 > out-file
+
+:- table inherit/2.
+
+inherit(A, B) :- 
+    parent(A, B).
+
+inherit(A, C) :-
+    parent(A, B),
+    inherit(B, C).
+
+diamond(A) :- 
+    parent(A, B1),
+    parent(A, B2),
+    B1 \= B2,
+    (
+        inherit(B1, C), inherit(B2, C)
+    ;
+        inherit(B1, B2)
+    ).
+
+diamond :-
+    diamond(_).
+
+do_case(Case_num) :-
+    ( diamond -> Result = "Yes" ; Result = "No" ),
+    format("Case #~w: ~s\n", [Case_num, Result]).
+    
+main :-
+    read([T]),
+    foreach(Case_num in 1..T, [N], (
+            initialize_table, abolish,
+            read([N]),
+            foreach(I in 1..N, [Parents, _M], (
+                    read([_M|Parents]),
+                    foreach(P in Parents, assert(parent(I, P)))
+                )),
+            do_case(Case_num)
+        )).
