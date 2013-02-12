8286b5fa5ec6f6600d67e2fa83ac4c090694bb3e
Sergey Dymchenko <kit1980@gmail.com>
2013-02-12 00:09:49 -0800

    Add B-Prolog solution and tests for '2009/Round 1C/C. Bribe the Prisoners'.

---
+++ 2009/Round 1C/C. Bribe the Prisoners/BribeThePrisoners.pl
@@ -0,0 +1,38 @@
+% Google Code Jam
+% Round 1C 2009
+% C. Bribe the Prisoners
+% https://code.google.com/codejam/contest/189252/dashboard#s=p2 
+%
+% Dynamic programming solution in B-Prolog.
+%
+% Author: Sergey Dymchenko <kit1980@gmail.com>
+%
+% B-Prolog 7.8#1 - http://www.probp.com/
+% Usage:
+% sed 's/^ */[/; s/ *$/]./; s/ \+/, /g' in-file | bp -g "['BribeThePrisoners.pl'],main,halt" -l | tail -n +5 > out-file
+
+:- table cost(+, +, min).
+
+cost(A, B, 0) :- 
+     \+ (ToFree :: A..B, free(ToFree)).
+
+cost(A, B, V) :- 
+    free(ToFree),
+    ToFree >= A, ToFree =< B,
+    Left is ToFree - 1, cost(A, Left, Vleft),
+    Right is ToFree + 1, cost(Right, B, Vright),
+    V is B - A + Vleft + Vright.
+
+do_case(Case_num, P) :-
+    cost(1, P, V),
+    format("Case #~w: ~w\n", [Case_num, V]).
+    
+main :-
+    read([N]),
+    foreach(Case_num in 1..N, [P, Q, Free], (
+                initialize_table, abolish,
+                read([P, Q]),
+                read(Free),
+                foreach(ToFree in Free, assert(free(ToFree))),
+                do_case(Case_num, P)
+           )).
